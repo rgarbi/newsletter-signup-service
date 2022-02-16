121275ae1a72a2273b8a2ac19b7e22c6242dd963
Richard Garbi <richard@mobilgov.com>
2022-02-16 08:59:53 -0600

    some more build out

--- src/db/subscriptions.rs
+++
@@ -1,5 +0,0 @@
-use crate::models::subscription::Subscription;
-
-pub async fn find_subscriptions_by_id(_id: i64) -> Option<Subscription> {
-    Option::from(Subscription::new_subscription())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-mod db;
 mod models;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
