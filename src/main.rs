9f0ad6da13346948bbcf0a1b0b27c30409734348
Richard Garbi <richard@mobilgov.com>
2022-02-22 07:16:00 -0600

    wire up logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 
 use newsletter_signup_service::configuration::get_configuration;
@@ -7,6 +8,7 @@ use newsletter_signup_service::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
