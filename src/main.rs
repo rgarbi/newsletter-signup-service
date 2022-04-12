cb97127ba9f00988044b506524d02ee37595ab86
Richard Garbi <richard@mobilgov.com>
2022-04-12 06:54:33 -0500

    remove some diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use secrecy::ExposeSecret;
+
 use newsletter_signup_service::configuration::get_configuration;
 use newsletter_signup_service::startup::Application;
 use newsletter_signup_service::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -13,14 +14,6 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    println!("Database host: {}", configuration.database.host);
-    println!("Database username: {}", configuration.database.username);
-    println!("Email base_url: {}", configuration.email_client.base_url);
-    println!(
-        "AUTH KEY: {}",
-        configuration.auth_config.signing_key.expose_secret()
-    );
-    println!("Email base_url: {}", configuration.email_client.base_url);
     let application = Application::build(configuration).await?;
     application.run_until_stopped().await?;
     Ok(())
