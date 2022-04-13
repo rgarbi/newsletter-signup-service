b0151817f3bcd08f7287d12d460e7eef0335f478
Richard Garbi <richard@mobilgov.com>
2022-04-13 05:10:11 -0500

    start removing the first name last name thing

--- migrations/20220221130541_create_subscribers_table.sql
+++ migrations/20220221130541_create_subscribers_table.sql
@@ -3,6 +3,5 @@ CREATE TABLE subscribers(
   id uuid NOT NULL,
   PRIMARY KEY (id),
   email_address TEXT NOT NULL UNIQUE,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL
+  name TEXT NOT NULL
 );
--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -5,8 +5,7 @@ use crate::domain::valid_name::ValidName;
 
 #[derive(Deserialize, Serialize)]
 pub struct OverTheWireCreateSubscriber {
-    pub first_name: String,
-    pub last_name: String,
+    pub name: String,
     pub email_address: String,
     pub user_id: String,
 }
@@ -20,15 +19,13 @@ impl OverTheWireCreateSubscriber {
 #[derive(Deserialize, Serialize)]
 pub struct OverTheWireSubscriber {
     pub id: String,
-    pub first_name: String,
-    pub last_name: String,
+    pub name: String,
     pub email_address: String,
     pub user_id: String,
 }
 
 pub struct NewSubscriber {
     pub email_address: ValidEmail,
-    pub first_name: ValidName,
-    pub last_name: ValidName,
+    pub name: ValidName,
     pub user_id: String,
 }
