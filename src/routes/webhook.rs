3e9c83d6084fffead5660b33e69960ab5029a742
Richard Garbi <richard@mobilgov.com>
2022-05-02 15:37:47 -0500

    cleaning up some warnings

--- src/routes/webhook.rs
+++ src/routes/webhook.rs
@@ -1,26 +1,11 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use core::option::Option;
 use secrecy::ExposeSecret;
 use serde_json::json;
 use sqlx::PgPool;
-use std::str::FromStr;
-use stripe::{
-    CheckoutSessionMode, Client, CreateCustomer, Customer, CustomerId, StripeError, Webhook,
-    WebhookEvent,
-};
 
-use crate::auth::token::Claims;
+use stripe::{Webhook, WebhookEvent};
+
 use crate::configuration::get_configuration;
-use crate::db::checkout_session_db_broker::{
-    insert_checkout_session, retrieve_checkout_session_by_stripe_session_id,
-    set_checkout_session_state_to_success_by_stripe_session_id,
-};
-use crate::db::subscribers_db_broker::{retrieve_subscriber_by_id, set_stripe_customer_id};
-use crate::db::subscriptions_db_broker::insert_subscription;
-use crate::domain::checkout_models::{CreateCheckoutSession, CreateCheckoutSessionRedirect};
-use crate::domain::subscriber_models::OverTheWireSubscriber;
-use crate::domain::subscription_models::{NewSubscription, OverTheWireCreateSubscription};
-use crate::util::from_string_to_uuid;
 
 #[tracing::instrument(
 name = "Handle Webhook",
@@ -34,7 +19,6 @@ pub async fn handle_webhook(
     req: HttpRequest,
     body: web::Bytes,
     _pool: web::Data<PgPool>,
-    _user: Claims,
 ) -> impl Responder {
     let configuration = get_configuration().unwrap();
     println!("Got a webhook event the ID was: {}", webhook_event.id);
