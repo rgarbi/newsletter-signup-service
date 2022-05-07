e9c4eeb26944a036609b0c7656c904fd03d0b496
Richard Garbi <richard@mobilgov.com>
2022-05-07 13:44:43 -0500

    fix some warnings

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,10 +2,10 @@ use crate::auth::token::Claims;
 use crate::db::subscribers_db_broker::retrieve_subscriber_by_id;
 use actix_web::{web, HttpResponse, Responder};
 use sqlx::PgPool;
+use uuid::Uuid;
 
-use crate::db::subscriptions_db_broker::{
-    retrieve_subscription_by_subscription_id, retrieve_subscriptions_by_subscriber_id,
-};
+use crate::db::subscriptions_db_broker::{cancel_subscription_by_subscription_id, retrieve_subscription_by_subscription_id, retrieve_subscriptions_by_subscriber_id};
+
 
 use crate::util::from_path_to_uuid;
 
@@ -50,14 +50,10 @@ pub async fn get_subscription_by_id(
 ) -> impl Responder {
     match retrieve_subscription_by_subscription_id(from_path_to_uuid(&id).unwrap(), &pool).await {
         Ok(subscription) => {
-            match retrieve_subscriber_by_id(subscription.subscriber_id, &pool).await {
-                Ok(subscriber) => {
-                    if subscriber.user_id != user.user_id {
-                        return HttpResponse::Unauthorized().finish();
-                    }
-                }
-                Err(_) => return HttpResponse::BadRequest().finish(),
-            }
+            match  reject_unauthorized_user(subscription.subscriber_id, user.user_id, &pool).await {
+                Ok(_) => {},
+                Err(response) => return response
+            };
             HttpResponse::Ok().json(subscription)
         }
         Err(_) => HttpResponse::NotFound().finish(),
@@ -76,22 +72,35 @@ pub async fn cancel_subscription_by_id(
     pool: web::Data<PgPool>,
     user: Claims,
 ) -> impl Responder {
-    match retrieve_subscription_by_subscription_id(from_path_to_uuid(&id).unwrap(), &pool).await {
+    let subscription_id = from_path_to_uuid(&id).unwrap();
+    match retrieve_subscription_by_subscription_id(subscription_id.clone(), &pool).await {
         Ok(subscription) => {
-            match retrieve_subscriber_by_id(subscription.subscriber_id, &pool).await {
-                Ok(subscriber) => {
-                    if subscriber.user_id != user.user_id {
-                        return HttpResponse::Unauthorized().finish();
-                    }
-                }
-                Err(_) => return HttpResponse::BadRequest().finish(),
-            }
+            match  reject_unauthorized_user(subscription.subscriber_id, user.user_id, &pool).await {
+                Ok(_) => {},
+                Err(response) => return response
+            };
+
+
+
             //Set it to active = false
+            cancel_subscription_by_subscription_id()
             //Add a history object....
             //Call stripe to cancel the subscription
 
             HttpResponse::Ok().json(subscription)
         }
         Err(_) => HttpResponse::NotFound().finish(),
+    }
+}
+
+async fn reject_unauthorized_user(subscriber_id: Uuid, user_id: String, pool: &PgPool) -> Result<(), HttpResponse> {
+    return match retrieve_subscriber_by_id(subscriber_id, &pool).await {
+        Ok(subscriber) => {
+            if subscriber.user_id != user_id {
+                return Err(HttpResponse::Unauthorized().finish());
+            }
+            Ok(())
+        }
+        Err(_) => Err(HttpResponse::BadRequest().finish()),
     }
 }
