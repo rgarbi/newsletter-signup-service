4f48aa54165a97924202dd104ed9a8957cfd3d38
Richard Garbi <richard@mobilgov.com>
2022-05-10 07:04:45 -0500

    structure the creation of the client

--- src/stripe_client/mod.rs
+++ src/stripe_client/mod.rs
@@ -1,7 +1,33 @@
-pub struct stripe_client {
-    pub base_url: String,
-    pub api_secret_key: String,
-    pub api_public_key: String,
-    pub webhook_secret: String,
-    pub
+use reqwest::Client;
+use secrecy::Secret;
+
+#[derive(Clone, Debug)]
+pub struct StripeClient {
+    http_client: Client,
+    base_url: String,
+    api_secret_key: Secret<String>,
+    api_public_key: Secret<String>,
+    webhook_secret: Secret<String>,
+}
+
+impl StripeClient {
+    pub fn new(
+        base_url: String,
+        api_secret_key: Secret<String>,
+        api_public_key: Secret<String>,
+        webhook_secret: Secret<String>,
+        timeout: std::time::Duration,
+    ) -> Self {
+        Self {
+            http_client: Client::builder()
+                .timeout(timeout)
+                .connection_verbose(true)
+                .build()
+                .unwrap(),
+            base_url,
+            api_secret_key,
+            api_public_key,
+            webhook_secret,
+        }
+    }
 }
