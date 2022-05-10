56b0adba2c2fd5f69b4cc9fdbd750622a3355305
Richard Garbi <richard@mobilgov.com>
2022-05-10 08:32:34 -0500

    start testing our client

--- src/stripe_client/mod.rs
+++ src/stripe_client/mod.rs
@@ -1,5 +1,9 @@
-use reqwest::Client;
-use secrecy::Secret;
+pub mod stripe_models;
+
+use crate::stripe_client::stripe_models::StripeSessionObject;
+use reqwest::{Client, Error};
+use secrecy::{ExposeSecret, Secret};
+use tracing::Level;
 
 #[derive(Clone, Debug)]
 pub struct StripeClient {
@@ -29,5 +33,90 @@ impl StripeClient {
             api_public_key,
             webhook_secret,
         }
+    }
+
+    #[tracing::instrument(
+        name = "Get Stripe Session By ID",
+        skip(stripe_session_id),
+        fields(
+        stripe_session_id = %stripe_session_id,
+        )
+    )]
+    pub async fn get_stripe_session(
+        &self,
+        stripe_session_id: String,
+    ) -> Result<StripeSessionObject, Error> {
+        let address = format!(
+            "{}/v1/checkout/sessions/{}",
+            &self.base_url, stripe_session_id
+        );
+
+        let response = self
+            .http_client
+            .get(address)
+            .basic_auth(
+                self.api_secret_key.expose_secret().to_string(),
+                Option::Some(String::new()),
+            )
+            .send()
+            .await;
+
+        return match response {
+            Ok(response) => {
+                let response_body = response.text().await.unwrap();
+                tracing::event!(Level::INFO, "Got the following back!! {:?}", &response_body);
+                let stripe_session: StripeSessionObject =
+                    serde_json::from_str(response_body.as_str()).unwrap();
+                Ok(stripe_session)
+            }
+            Err(err) => {
+                tracing::event!(Level::ERROR, "Err: {:?}", err);
+                Err(err)
+            }
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::{assert_err, assert_ok};
+    use fake::{Fake, Faker};
+    use secrecy::Secret;
+
+    use uuid::Uuid;
+    use wiremock::matchers::{any, header, header_exists, method, path};
+    use wiremock::{Mock, MockServer, Request, ResponseTemplate};
+
+    use crate::stripe_client::StripeClient;
+
+    fn stripe_client(base_url: String) -> StripeClient {
+        StripeClient::new(
+            base_url,
+            Secret::new(Faker.fake()),
+            Secret::new(Faker.fake()),
+            Secret::new(Faker.fake()),
+            std::time::Duration::from_millis(200),
+        )
+    }
+
+    #[tokio::test]
+    async fn get_stripe_session_fires_a_request_to_base_url() {
+        // Arrange
+        let session_id = Uuid::new_v4().to_string();
+        let mock_server = MockServer::start().await;
+        let stripe_client = stripe_client(mock_server.uri());
+        Mock::given(header_exists("Authorization"))
+            .and(header("Content-Type", "application/json"))
+            .and(path(format!("v1/checkout/sessions/{}", &session_id)))
+            .and(method("GET"))
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = stripe_client.get_stripe_session(session_id).await;
+        // Assert
+        assert_ok!(outcome);
     }
 }
---
+++ src/stripe_client/stripe_models.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct StripeBillingPortalSession {
+    pub id: String,
+    pub object: String,
+    pub configuration: String,
+    pub created: u128,
+    pub customer: String,
+    pub livemode: bool,
+    pub locale: Option<String>,
+    pub on_behalf_of: Option<String>,
+    pub return_url: String,
+    pub url: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct StripeSessionObject {
+    pub id: String,
+    pub object: String,
+    pub amount_subtotal: u128,
+    pub amount_total: u128,
+    pub client_reference_id: Option<String>,
+    pub customer: String,
+    pub subscription: Option<String>,
+}
+
+impl StripeSessionObject {
+    pub fn to_json(&self) -> String {
+        serde_json::to_string(self).expect("Was not able to serialize.")
+    }
+}
