b897d5d5af25731d216c47705d059a088c6b731e
Richard Garbi <richard@mobilgov.com>
2022-05-12 11:43:15 -0500

    remove the stripe client and webhook validation

--- src/routes/webhook.rs
+++ src/routes/webhook.rs
@@ -1,9 +1,6 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use secrecy::ExposeSecret;
 use serde_json::json;
 use sqlx::PgPool;
-
-use stripe::Webhook;
 
 use crate::configuration::get_configuration;
 
@@ -13,33 +10,14 @@ pub async fn handle_webhook(
     body: web::Bytes,
     _pool: web::Data<PgPool>,
 ) -> impl Responder {
-    let configuration = get_configuration().unwrap();
+    let _configuration = get_configuration().unwrap();
     let stripe_signature_header = req.headers().get("Stripe-Signature");
 
     if let Some(..) = stripe_signature_header {
-        let signature = stripe_signature_header.unwrap().to_str().ok().unwrap();
-        let body = std::str::from_utf8(&body).unwrap();
+        let _signature = stripe_signature_header.unwrap().to_str().ok().unwrap();
+        let _body = std::str::from_utf8(&body).unwrap();
 
-        let validate_signature = Webhook::construct_event(
-            body,
-            signature,
-            configuration
-                .stripe_client
-                .webhook_key
-                .expose_secret()
-                .as_str(),
-        );
-
-        match validate_signature {
-            Ok(webhook_event) => {
-                println!("Successfully validated the webhook!!!");
-                println!("Web hook type was: {:?}", webhook_event.event_type);
-                println!("Web hook object was: {:?}", webhook_event.data.object);
-            }
-            Err(webhook_error) => {
-                println!("Err: {:?}", webhook_error);
-            }
-        }
+        println!("Got a webhook")
     }
 
     HttpResponse::Ok().json(json!({}))
