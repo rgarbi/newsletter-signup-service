ff5cbc3382974bd7891ef976f13a14266bd848cf
Richard Garbi <richard@mobilgov.com>
2022-05-13 11:26:27 -0500

    add another test

--- tests/api/otp_db_test.rs
+++ tests/api/otp_db_test.rs
@@ -1,5 +1,5 @@
 use chrono::{Duration, Utc};
-use claim::assert_ok;
+use claim::{assert_err, assert_ok};
 use uuid::Uuid;
 
 use newsletter_signup_service::db::otp_db_broker::{
@@ -27,6 +27,24 @@ async fn insert_otp_works() {
 }
 
 #[tokio::test]
+async fn insert_duplicate_otp_blows_up() {
+    let app = spawn_app().await;
+
+    let otp = OneTimePasscode {
+        id: Uuid::new_v4(),
+        user_id: Uuid::new_v4().to_string(),
+        one_time_passcode: Uuid::new_v4().to_string(),
+        issued_on: Utc::now(),
+        expires_on: Utc::now() + Duration::seconds(100),
+        used: false,
+    };
+
+    let _discard = insert_otp(otp.clone(), &app.db_pool).await;
+    let result = insert_otp(otp.clone(), &app.db_pool).await;
+    assert_err!(result);
+}
+
+#[tokio::test]
 async fn get_otp_by_otp_works() {
     let app = spawn_app().await;
 
@@ -45,6 +63,14 @@ async fn get_otp_by_otp_works() {
     let saved = get_otp_by_otp(otp.one_time_passcode.as_str(), &app.db_pool).await;
     assert_ok!(&saved);
     assert_eq!(otp.one_time_passcode, saved.unwrap().one_time_passcode);
+}
+
+#[tokio::test]
+async fn get_otp_by_otp_blows_up_when_not_found() {
+    let app = spawn_app().await;
+
+    let saved = get_otp_by_otp(Uuid::new_v4().to_string().as_str(), &app.db_pool).await;
+    assert_err!(saved);
 }
 
 #[tokio::test]
