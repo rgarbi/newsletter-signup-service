6930da0bfd8ed779ad9ede25eb8b2cc385f571b2
Richard Garbi <richard@mobilgov.com>
2022-05-17 13:12:53 -0500

    fix test and add a new test

--- tests/api/subscriptions_tests.rs
+++ tests/api/subscriptions_tests.rs
@@ -149,6 +149,35 @@ async fn cancel_subscription_by_id() {
 
 }
 
+#[tokio::test]
+async fn cancel_subscription_by_id_not_authorized() {
+    let app = spawn_app().await;
+
+    let subscriber = app.store_subscriber(Option::None).await;
+    let stored_subscription = store_subscription(subscriber.id.to_string(), &app).await;
+
+    let subscriptions_response = app
+        .get_subscription_by_id(
+            stored_subscription.id.to_string(),
+            generate_token(subscriber.user_id.clone()),
+        )
+        .await;
+    assert_eq!(200, subscriptions_response.status().as_u16());
+
+    let cancel_subscription_response = app.cancel_subscription_by_id(stored_subscription.id.to_string(), generate_token(Uuid::new_v4().to_string())).await;
+    assert_eq!(401, cancel_subscription_response.status().as_u16());
+
+}
+
+#[tokio::test]
+async fn cancel_subscription_by_id_not_found() {
+    let app = spawn_app().await;
+
+    let cancel_subscription_response = app.cancel_subscription_by_id(Uuid::new_v4().to_string(), generate_token(Uuid::new_v4().to_string())).await;
+    assert_eq!(404, cancel_subscription_response.status().as_u16());
+
+}
+
 async fn store_subscription(subscriber_id: String, app: &TestApp) -> OverTheWireSubscription {
     let over_the_wire_create_subscription =
         generate_over_the_wire_create_subscription(subscriber_id);
