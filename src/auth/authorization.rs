3616b0c1eb1c6c360a837987991937c821b30754
Richard Garbi <richard@mobilgov.com>
2022-05-30 15:08:26 -0500

    add another method and some tests

--- src/auth/authorization.rs
+++ src/auth/authorization.rs
@@ -1,16 +1,36 @@
 use crate::auth::token::Claims;
 use crate::domain::user_models::UserGroup;
+use tracing::Level;
 
 pub fn is_authorized_admin_only(user_id: String, token: Claims) -> bool {
     if (user_id == token.user_id) && token.group == UserGroup::ADMIN.as_str() {
         return true;
     }
+    tracing::event!(
+        Level::ERROR,
+        "User ID: {} is not authorized on an admin API",
+        user_id
+    );
+    return false;
+}
+
+pub fn is_authorized_user_or_admin(user_id: String, token: Claims) -> bool {
+    if (user_id == token.user_id)
+        && (token.group == UserGroup::ADMIN.as_str() || token.group == UserGroup::USER.as_str())
+    {
+        return true;
+    }
+    tracing::event!(
+        Level::ERROR,
+        "User ID: {} is not authorized on an admin API",
+        user_id
+    );
     return false;
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::auth::authorization::is_authorized_admin_only;
+    use crate::auth::authorization::{is_authorized_admin_only, is_authorized_user_or_admin};
     use crate::auth::token::Claims;
     use uuid::Uuid;
 
@@ -74,6 +94,80 @@ mod tests {
                 Claims {
                     user_id: Uuid::new_v4().to_string(),
                     group: "USER".to_string(),
+                    iss: "".to_string(),
+                    aud: "".to_string(),
+                    sub: "".to_string(),
+                    exp: 0,
+                    iat: 0,
+                }
+            )
+        );
+
+        assert_eq!(
+            false,
+            is_authorized_admin_only(
+                user_id.clone(),
+                Claims {
+                    user_id: Uuid::new_v4().to_string(),
+                    group: Uuid::new_v4().to_string(),
+                    iss: "".to_string(),
+                    aud: "".to_string(),
+                    sub: "".to_string(),
+                    exp: 0,
+                    iat: 0,
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn is_authorized_admin_or_user_passes() {
+        let user_id = Uuid::new_v4().to_string();
+
+        assert_eq!(
+            true,
+            is_authorized_user_or_admin(
+                user_id.clone(),
+                Claims {
+                    user_id: user_id.clone(),
+                    group: "ADMIN".to_string(),
+                    iss: "".to_string(),
+                    aud: "".to_string(),
+                    sub: "".to_string(),
+                    exp: 0,
+                    iat: 0,
+                }
+            )
+        );
+
+        assert_eq!(
+            true,
+            is_authorized_user_or_admin(
+                user_id.clone(),
+                Claims {
+                    user_id: user_id.clone(),
+                    group: "USER".to_string(),
+                    iss: "".to_string(),
+                    aud: "".to_string(),
+                    sub: "".to_string(),
+                    exp: 0,
+                    iat: 0,
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn is_authorized_admin_or_user_does_not_pass() {
+        let user_id = Uuid::new_v4().to_string();
+
+        assert_eq!(
+            false,
+            is_authorized_user_or_admin(
+                user_id.clone(),
+                Claims {
+                    user_id: Uuid::new_v4().to_string(),
+                    group: "ADMIN".to_string(),
                     iss: "".to_string(),
                     aud: "".to_string(),
                     sub: "".to_string(),
