4671ac90a51d4e938e41009171e3c4bc0ad98df6
Richard Garbi <richard@mobilgov.com>
2022-06-19 19:36:35 -0500

    more work on setting up the test

--- tests/api/subscription_notifier_test.rs
+++ tests/api/subscription_notifier_test.rs
@@ -1,5 +1,7 @@
 use chrono::Utc;
 use claim::{assert_err, assert_ok};
+use fake::Faker;
+use secrecy::Secret;
 use newsletter_signup_service::background::new_subscription_notifier::notify_of_new_subscription;
 use newsletter_signup_service::db::subscribers_db_broker::{
     insert_subscriber, retrieve_subscriber_by_user_id,
@@ -13,6 +15,7 @@ use newsletter_signup_service::domain::subscription_models::{
     OverTheWireSubscription, SubscriptionType,
 };
 use uuid::Uuid;
+use newsletter_signup_service::email_client::EmailClient;
 
 use crate::helper::{
     generate_new_subscription, generate_over_the_wire_subscriber, spawn_app, store_subscription,
@@ -21,6 +24,7 @@ use crate::helper::{
 #[tokio::test]
 async fn notify_of_new_subscriber_works() {
     let app = spawn_app().await;
+    let mock_email_client = app.email_server
 
     let subscriber = generate_over_the_wire_subscriber();
     let new_subscriber: NewSubscriber = subscriber.clone().try_into().unwrap();
@@ -35,5 +39,14 @@ async fn notify_of_new_subscriber_works() {
             .unwrap();
     let stored_subscription = store_subscription(subscriber.id.to_string(), None, &app).await;
 
-    notify_of_new_subscription(stored_subscription)
+    notify_of_new_subscription(stored_subscription.id.clone())
+}
+
+fn email_client(base_url: String) -> EmailClient {
+    EmailClient::new(
+        base_url,
+        email(),
+        Secret::new(Faker.fake()),
+        std::time::Duration::from_millis(200),
+    )
 }
