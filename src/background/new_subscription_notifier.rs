8bcad3eae4e6bc131fee700c94efb00501abc515
Richard Garbi <richard@mobilgov.com>
2022-06-22 07:47:47 -0500

    add the email message text body

--- src/background/new_subscription_notifier.rs
+++ src/background/new_subscription_notifier.rs
@@ -1,5 +1,6 @@
 use crate::configuration::get_configuration;
 use crate::db::subscriptions_db_broker::retrieve_subscription_by_subscription_id;
+use crate::domain::subscription_models::OverTheWireSubscription;
 use crate::domain::valid_email::ValidEmail;
 use crate::email_client::EmailClient;
 use sqlx::PgPool;
@@ -33,4 +34,26 @@ pub async fn notify_subscriber(subscription_id: Uuid, email_client: EmailClient,
             .await
             .is_ok();
     }
+}
+
+fn text_content(subscription: OverTheWireSubscription) -> String {
+    format!(
+        r#"A new subscription was created. The details are below:
+        Subscription Type: {}
+        Subscription Name: {}
+        Subscription Address Line 1: {}
+        Subscription Address Line 2: {}
+        Subscription City: {}
+        Subscription State: {},
+        Subscription Postal Code: {},
+        SUbscription Date: {}"#,
+        subscription.subscription_type.as_str(),
+        subscription.subscription_name,
+        subscription.subscription_mailing_address_line_1,
+        subscription.subscription_mailing_address_line_2,
+        subscription.subscription_city,
+        subscription.subscription_state,
+        subscription.subscription_postal_code,
+        subscription.subscription_creation_date
+    )
 }
